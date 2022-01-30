f1fde2b41096fa41c47a365440be3e1439305e0e
Pasha831 <46136468+Pasha831@users.noreply.github.com>
2022-01-30 16:34:07 +0300

    Changed main.c (stack, queue, precedence) and added new files to .gitignore

--- main.c
+++ main.c
@@ -1,6 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXSIZE 1000  // max size of input string
+
+FILE *fr;
+FILE *fw;
+
+// is an operator?
+int inOperators(const char *inp) {
+    // we have to add other operators, like: '(', ')', '^'
+    if (*inp == '+' || *inp == '-' || *inp == '*' || *inp == '/') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+// clean the number string and set z (pointer) to 0
+void cleanNumber(char *n, int *z) {
+    for (int i = 0; i < *z; i++) {
+        n[i] = '\0';
+    }
+    *z = 0;
+}
+
+/* return the precedence (priority) of operator:
+    '(', ')' -> 1
+    '^' -> 2
+    '*', '/' -> 3
+    '+', '-' -> 4
+*/
+int precedence(const char* op) {
+    if (*op == '(' || *op == ')') {
+        return 1;
+    }
+    else if (*op == '^') {
+        return 2;
+    }
+    else if (*op == '*' || *op == '/') {
+        return 3;
+    }
+    else if (*op == '+' || *op == '-') {
+        return 4;
+    }
+}
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    // input here your own files destination
+    fr = fopen("C:\\Users\\medve\\CLionProjects\\c-calc\\c-calculator\\input.txt", "r");
+    fw = fopen("C:\\Users\\medve\\CLionProjects\\c-calc\\c-calculator\\output.txt", "w");
+
+    char inp[MAXSIZE] = { 0 };  // each line of input
+
+    char stack[MAXSIZE][MAXSIZE] = { 0 };
+    char queue[MAXSIZE][MAXSIZE] = { 0 };
+    int k = 0;  // pointer for stack
+    int m = 0;  // pointer for queue
+
+    char n[MAXSIZE] = { 0 };  // to store long numbers from input
+    int z = 0;  // pointer for number
+
+    while (fgets(inp, MAXSIZE, fr)) {
+        for (int i = 0; i < strlen(inp) - 1; i++) {  // last char of a line is '\n' - don't make sense for us
+            if (inp[i] >= '0' && inp[i] <= '9') {  // is a digit?
+                n[z++] = inp[i];
+            }
+            else if (inOperators(&inp[i])) {  // is an operator?
+                if (z != 0) {  // if number is not empty
+                    strcat(queue[m++], n);
+                    cleanNumber(n, &z);
+                }
+
+                if (k == 0) {  // if stack is empty
+                    strcat(stack[k++], &inp[i]);
+                }
+                else {
+
+                }
+            }
+        }
+    }
 }
