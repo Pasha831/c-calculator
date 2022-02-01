72d1372535fe77868b7aef6423beb9865fd63331
maxveega <max-im.ageev@yandex.ru>
2022-02-02 00:07:05 +0300

    added first unary operations

--- main.c
+++ main.c
@@ -7,6 +7,14 @@
 FILE *fr;
 FILE *fw;
 
+// abs function for double (absolute value), (not working, because we don't work with unary minus)
+double absd(double x){
+    if (x < 0){
+        return -x;
+    }
+    return x;
+}
+
 // is an operator?
 int inOperators(const char *inp) {
     // we have to add other operators, like: '(', ')'
@@ -18,6 +26,7 @@ int inOperators(const char *inp) {
     }
 }
 
+// adding new number from input to RPL
 void getNumber(char* inp, char polish[MAXSIZE][MAXSIZE], int *m, int *i){
     int count = 0;
     while ((inp[*i] >= '0' && inp[*i] <= '9') || inp[*i] == '.' || inp[*i] == ',') {  // is a part of a digit?
@@ -31,11 +40,33 @@ void getNumber(char* inp, char polish[MAXSIZE][MAXSIZE], int *m, int *i){
     (*i)--;
 }
 
-// push number to queue, then clean the number string and set z (pointer) to 0
-void addNumber(char polish[MAXSIZE][MAXSIZE], char *n, int *z, int *m) {
-    n[*z] = 0;
-    strcpy(polish[(*m)++], n);
-    *z = 0;
+// adding symbols (of function or variable) from input to string
+void getSymbols(char* inp, char* str, int* i){
+    int count = 0;
+    while (!inOperators(&inp[*i])){  // reading symbols from input until an operator is found
+        str[count++] = inp[(*i)++];
+    }
+    str[count] = 0;
+    (*i)--;
+}
+
+// returns an index of a function from the list or -1 if function is not in the list
+int inFunctions(char* str){
+    char* functions[] = { "cos",
+                          "sin",
+                          "tg",
+                          "log",
+                          "ln",
+                          "sqrt",
+                          "abs",
+                          "exp"};
+    //have to add "pow", "real", "imag", "mag", "phase"
+    for (int i = 0; i < 8; ++i){
+        if (!strcmp(functions[i], str)){
+            return i;
+        }
+    }
+    return -1;
 }
 
 /* return the precedence (priority) of operator:
@@ -64,9 +95,6 @@ void createRPN(char polish[MAXSIZE][MAXSIZE], int *m, char inp[MAXSIZE]) {
     char stack[MAXSIZE][MAXSIZE] = { 0 };  // operation stack
     int k = 0;  // pointer for stack
 
-    char n[MAXSIZE] = { 0 };  // to store long numbers from input
-    int z = 0;  // pointer for number
-
     for (int i = 0; i < strlen(inp); i++) {
         if ((inp[i] >= '0' && inp[i] <= '9') || inp[i] == '.' || inp[i] == ',') {  // is a part of a digit?
             getNumber(inp, polish, m, &i);
@@ -75,9 +103,6 @@ void createRPN(char polish[MAXSIZE][MAXSIZE], int *m, char inp[MAXSIZE]) {
             char operator[MAXSIZE] = { 0 };
             operator[0] = inp[i];
 
-            /*f (z != 0) {  // if number is not empty
-                addNumber(polish, n, &z, m);
-            }*/
 
             if (k == 0) {  // if stack is empty
                 strcpy(stack[k++], operator);
@@ -93,7 +118,10 @@ void createRPN(char polish[MAXSIZE][MAXSIZE], int *m, char inp[MAXSIZE]) {
                             strcpy(polish[(*m)++], stack[k]);  // pop operator
                             strcpy(stack[k], "\0");  // clean the position
                         }
-                        strcpy(stack[k], "\0");  // when we reached '(': clean '('
+                        strcpy(stack[k], "\0");// when we reached '(': clean '('
+                        if (inFunctions(stack[k-1]) != -1){  // if there is a function before '('
+                            strcpy(polish[(*m)++], stack[--k]);  // pop function
+                        }
                         break;  // then break the search
                     }
                     else if (precedence(&inp[i]) < precedence(stack[k - 1]) || stack[k - 1][0] == '(') {  // if previous operator is '(' - add it anyway
@@ -109,11 +137,15 @@ void createRPN(char polish[MAXSIZE][MAXSIZE], int *m, char inp[MAXSIZE]) {
                 }
             }
         }
+        else{
+            char str[MAXSIZE] = { 0 };
+            getSymbols(inp, str, &i);
+            if (inFunctions(str) != -1){  // if str is a function
+                strcpy(stack[k++], str);  // adding to operation stack
+            }
+        }
     }
 
-    /*if (z != 0) {  // push the remaining number, if there is one
-        addNumber(polish, n, &z, m);
-    }*/
     while (k > 0) {  // push the remaining operations
         strcpy(polish[(*m)++], stack[--k]);
     }
@@ -124,13 +156,21 @@ double calculateRPN(char polish[MAXSIZE][MAXSIZE], int n) {
 
     double* stack = (double*)calloc(n, sizeof(double));
     int count = 0;
+    double (*func[])(double) = {cos,
+                                sin,
+                                tan,
+                                log10,
+                                log,
+                                sqrt,
+                                absd,
+                                exp};
 
     for (int i = 0; i < n; ++i){
         if (polish[i][0] >= '0' && polish[i][0] <= '9'){
             stack[count] = strtod(polish[i], 0);
             count++;
         }
-        else{
+        else if (inOperators(&polish[i][0])){
             if (polish[i][0] == '+') {
                 stack[count - 2] += stack[count - 1];
             } else if (polish[i][0] == '-') {
@@ -144,6 +184,12 @@ double calculateRPN(char polish[MAXSIZE][MAXSIZE], int n) {
             }
             stack[count - 1] = 0;
             count--;
+        }
+        else {
+            int index = inFunctions(polish[i]);
+            if (index != -1) {
+                stack[count - 1] = func[index](stack[count - 1]);
+            }
         }
     }
 
